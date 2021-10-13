8c50a393395d31a50be78e8b3bcda01843a66262
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2021-10-13 22:34:08 +0300

    Separates words from sentences.

--- Sentence-And-Binary-Code.cpp
+++ Sentence-And-Binary-Code.cpp
@@ -1,16 +1,45 @@
 using namespace std;
 #include <iostream>
+#include <string>
 #include "Node.h"
 
+template<typename T>
+void AddNtoN(Node<T>* head, T N, T toN) {
+	Node<int>::NodeOperations NodeOp;
+	//Node<T>* head = new Node<T>();
+	for (int i = N; i <= toN; i++)
+		NodeOp.ImportAtTheEnd(&head, (T)i);
+}
+
+//void AddNtoNExample() {
+//	Node<int>::NodeOperations NodeOp;
+//
+//	Node<int>* head = new Node<int>();
+//	AddNtoN<int>(head, 1, 20);
+//	NodeOp.PrintList(head);
+//}
+
+Node<string>* SeperateWords(string line) {
+	Node<string>::NodeOperations NodeOp;
+	Node<string>* head = new Node<string>();
+
+	string word;
+	for (int i = 0; i < line.length(); i++)
+		if (line[i] != ' ')
+			word += line[i];
+		else {
+			NodeOp.ImportAtTheEnd(&head, word);
+			word = "";
+		}
+
+	return head;
+}
+
 int main() {
-	Node<char>::NodeOperations NodeOp;
+	Node<string>::NodeOperations NodeOp;
 
-	Node<char>* head = new Node<char>();
-	for (int i = 65; i <= 75; i++)
-		NodeOp.ImportAtTheEnd(&head, (char)i);
-	NodeOp.PrintList(head);
-
-	cout << '\n' << "-------------------" << '\n';
-
-	NodeOp.ExampleMethod();
+	string line;
+	getline(cin, line);
+	Node<string>* wordsList = SeperateWords(line + " ");
+	NodeOp.PrintList(wordsList);
 }
