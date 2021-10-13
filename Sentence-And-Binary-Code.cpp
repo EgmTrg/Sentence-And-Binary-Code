78f8f0005d749301fdcefa270d788a4692499b0e
Egemen Turoglu <62505792+EgmTrg@users.noreply.github.com>
2021-10-13 15:55:45 +0300

    Created Node.h file and relocated Node class from Main.cpp file to Node.h file
    Created Node.h file and relocated Node class from Main.cpp file to Node.h file

--- Sentence-And-Binary-Code.cpp
+++ Sentence-And-Binary-Code.cpp
@@ -1,59 +1,6 @@
 using namespace std;
 #include <iostream>
-
-class Node {
-public:
-	int Value;
-	Node* Next;
-
-	class NodeOperations {
-	public:
-		void ExampleMethod() {
-			Node::NodeOperations nodeOp;
-			Node* head = new Node();
-			Node* second = new Node();
-			Node* third = new Node();
-
-			head->Value = 1;
-			head->Next = second;
-			second->Value = 2;
-			second->Next = third;
-			third->Value = 3;
-			third->Next = NULL;
-
-			nodeOp.ImportAtTheEnd(&head, 4);
-			nodeOp.PrintList(head);
-		}
-		void PrintList(Node* node) {
-			while (node != NULL)
-			{
-				cout << node->Value << endl;
-				node = node->Next;
-			}
-		}
-		void ImportAtTheEnd(Node** head, int newValue) {
-			// Create a new node for import.
-			Node* newNode = new Node();
-			newNode->Value = newValue;
-			newNode->Next = NULL;
-
-			// Be head if has no head.
-			if (*head == NULL)
-			{
-				*head = newNode;
-				return;
-			}
-
-			// Find the last node.
-			Node* last = *head;
-			while (last->Next != NULL)
-				last = last->Next;
-
-			// Import the new node to last node->next
-			last->Next = newNode;
-		}
-	};
-};
+#include "Node.h"
 
 int main() {
 	Node::NodeOperations nodeOp;
